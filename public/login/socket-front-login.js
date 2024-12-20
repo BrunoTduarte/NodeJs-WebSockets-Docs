713f6d8934ed75a67b867fafe6502ae6ebe31434
Bruno Tomé <btomeduarte@gmail.com>
2024-12-20 12:50:30 -0300

    Feat: sistema de cookie

--- public/login/socket-front-login.js
+++ public/login/socket-front-login.js
@@ -1,10 +1,14 @@
+import { definirCookie } from "../utils/cookies.js";
+
 const socket = io();
 
 function emitirAutenticarUsuario(dados){
     socket.emit("autenticar_usuario", dados);
 }
 
-socket.on("autenticado_sucesso", () =>{
+socket.on("autenticado_sucesso", (tokenJwt) =>{
+    definirCookie("tokenJwt", tokenJwt);
+
     (alert("Autenticação ocorrida com sucesso"))
     window.location.replace("/");
 
