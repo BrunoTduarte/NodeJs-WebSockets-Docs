5a8d8bc6234a6347f40fe4817982711234896223
Bruno Tomé <btomeduarte@gmail.com>
2025-01-02 17:18:01 -0300

    Feat: Cookie

--- servidor/socket-back.js
+++ servidor/socket-back.js
@@ -4,11 +4,20 @@ import registrarEventosCadastro from "./registrarEventos/Cadastro.js";
 import registratEventosDocumento from "./registrarEventos/Documento.js";
 import registraEventosInicio from "./registrarEventos/Inicio.js";
 import  registrarEventosLogin  from "./registrarEventos/Login.js";
-import io from "./servidor.js";
 
-io.on("connection", (socket) => {
+import io from "./servidor.js";
+import autorizarUsuario from "./middlewares/autorizarUsuario.js";
+
+const nspUsuario = io.of("/usuarios")
+
+nspUsuario.use(autorizarUsuario);
+
+nspUsuario.on("connection", (socket) => {
+  registraEventosInicio(socket, nspUsuario);
+  registratEventosDocumento(socket, nspUsuario);
+})
+
+io.of("/").on("connection", (socket) => {
   registrarEventosCadastro(socket,io);
   registrarEventosLogin(socket, io);
-  registraEventosInicio(socket,io);
-  registratEventosDocumento(socket, io);
 }); 
